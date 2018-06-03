e3b063b62d77df91ddcc9c330f106402a5d352dd
João Soares <joao.soares@avantsoft.com.br>
2018-06-03 20:11:22 +0200

    Refactor get_point_norm_gradient

--- src/gray_level_profile.py
+++ src/gray_level_profile.py
@@ -13,23 +13,42 @@ class GrayLevelProfile:
         return samples
 
     @classmethod
-    def from_image(cls, image, center_point, direction_vector,
-                   half_sampling_size):
-        """Creates a gray level profile of an image on the specified location"""
-        # Translate POI to origin and rotate around so direction vector is horizontal
-        angle = np.degrees(
-            -np.arctan2(direction_vector[1], direction_vector[0])
-        )  # invert because np fn uses (1, 0) as ref
-        print(angle)
-        rows, cols = image.shape
 
-        M = cv2.getRotationMatrix2D(center_point, angle, 1)
-        rot_img = cv2.warpAffine(image, M, (cols, rows))
-
-        x_center, y_center = (rows // 2, cols // 2)
+    @classmethod
+    def get_point_norm_gradient(cls, image, center_point, direction_vector,
+                                half_sampling_size) -> np.ndarray:
+        """Extracts the normalized gradient of a vector along the specified
+        direction and centerpoint of an image.
+        """
+        rot_img, x_center, y_center = cls.rotate_and_center_image(
+            image, center_point, direction_vector)
         # Create array spanning from (0, -k) to (0, k)
         samples = rot_img[x_center, (y_center - half_sampling_size):(
             y_center + half_sampling_size + 1)]
         print(samples)
-        glp = cls(samples)
-        return (glp, rot_img)
+
+        # Take derivative and normalize
+        norm_sample_gradients = cls.normalize(np.gradient(samples))
+        return norm_sample_gradients
+
+    @staticmethod
+    def normalize(samples):
+        """Normalizes sample by dividing through by sum of absolute element values"""
+        return samples / sum(samples)
+
+    @staticmethod
+    def rotate_and_center_image(image, center_point, direction_vector):
+        rows, cols = image.shape
+        # Translate POI to origin and rotate around so direction vector is horizontal
+        angle = np.degrees(
+            -np.arctan2(direction_vector[1], direction_vector[0])
+        )  # invert because np fn uses (1, 0) as ref
+
+        # Create rotation matrix and apply warp affine
+        M = cv2.getRotationMatrix2D(center_point, angle, 1)
+        rot_img = cv2.warpAffine(image, M, (cols, rows))
+
+        # Find new image center
+        x_center, y_center = (rows // 2, cols // 2)
+
+        return (rot_img, x_center, y_center)
