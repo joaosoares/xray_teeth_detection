511bbf3b87f5741d931aa0ccd5afd792790330af
João Soares <joao.soares@avantsoft.com.br>
2018-06-13 21:46:43 +0200

    Format data preprocessing class and add defaults to bilateral

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,12 +1,17 @@
 import cv2
 from matplotlib import pyplot as plt
-class Preprocessor():
 
+
+class Preprocessor:
     @classmethod
-    def bilateral_filter(cls, img, diameter, sigma_color, sigma_space, times):
-        filtered = img
-        for i in range(times):
-            filtered = cv2.bilateralFilter(img, d=diameter, sigmaColor=sigma_color, sigmaSpace=sigma_space)
+    def bilateral_filter(
+        cls, image, diameter=9, sigma_color=150, sigma_space=150, times=1
+    ):
+        filtered = image
+        for _ in range(times):
+            filtered = cv2.bilateralFilter(
+                image, d=diameter, sigmaColor=sigma_color, sigmaSpace=sigma_space
+            )
         return filtered
 
     @classmethod
@@ -42,8 +47,26 @@ class Preprocessor():
     def apply_sobel(cls, img, scale, delta):
         ddepth = cv2.CV_16S
 
-        grad_x = cv2.Sobel(img, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
-        grad_y = cv2.Sobel(img, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+        grad_x = cv2.Sobel(
+            img,
+            ddepth,
+            1,
+            0,
+            ksize=3,
+            scale=scale,
+            delta=delta,
+            borderType=cv2.BORDER_DEFAULT,
+        )
+        grad_y = cv2.Sobel(
+            img,
+            ddepth,
+            0,
+            1,
+            ksize=3,
+            scale=scale,
+            delta=delta,
+            borderType=cv2.BORDER_DEFAULT,
+        )
 
         abs_grad_x = cv2.convertScaleAbs(grad_x)
         abs_grad_y = cv2.convertScaleAbs(grad_y)
@@ -53,23 +76,27 @@ class Preprocessor():
     @classmethod
     def apply_scharr(cls, img, scale, delta):
         ddepth = cv2.CV_16S
-        grad_x = cv2.Scharr(img, ddepth, 1, 0, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
-        grad_y = cv2.Scharr(img, ddepth, 0, 1, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+        grad_x = cv2.Scharr(
+            img, ddepth, 1, 0, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT
+        )
+        grad_y = cv2.Scharr(
+            img, ddepth, 0, 1, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT
+        )
 
         abs_grad_x = cv2.convertScaleAbs(grad_x)
         abs_grad_y = cv2.convertScaleAbs(grad_y)
 
         return cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
 
-if __name__ == '__main__':
 
-    img = cv2.imread('../data/Radiographs/01.tif', flags=cv2.IMREAD_GRAYSCALE)
-    img = Preprocessor.bilateral_filter(img, diameter=9, sigma_color=150, sigma_space=150, times=1)
-    #img = Preprocessor.median_filter(img, ksize=5, times=5)
-    #img = Preprocessor.top_hat_processing(img, ksize=150)
+if __name__ == "__main__":
+
+    img = cv2.imread("../data/Radiographs/01.tif", flags=cv2.IMREAD_GRAYSCALE)
+    img = Preprocessor.bilateral_filter(
+        img, diameter=9, sigma_color=150, sigma_space=150, times=1
+    )
+    # img = Preprocessor.median_filter(img, ksize=5, times=5)
+    # img = Preprocessor.top_hat_processing(img, ksize=150)
     img = Preprocessor.apply_sobel(img, scale=1, delta=0)
 
-
     Preprocessor.show_image(img)
-
-
